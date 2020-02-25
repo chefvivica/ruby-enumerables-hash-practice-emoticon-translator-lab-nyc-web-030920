c97933295d0cb1686a0a2707af2db70bbe20ee45
Vivica Ma <vivicacongma@gmail.com>
2020-02-25 19:04:37 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -19,7 +19,7 @@ end
 
 def get_japanese_emoticon(file_path, emoticon)
     result = load_library(file_path)['get_emoticon'][emoticon]
-    if result === emoticon 
+    if result
       return result
     else 
       puts "Sorry, that emoticon was not found"
